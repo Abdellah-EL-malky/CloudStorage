a6394154a287a000ac97829ef016a751b4610f89
Abdellah <abdel.elmalky@gmail.com>
2025-04-14 16:19:14 +0100

    création de la database

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">Example Component</div>
+            <div class="card-body">
+                Je suis un exemple de composant Vue.js.
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('Component mounted.')
+    }
+}
+</script>
