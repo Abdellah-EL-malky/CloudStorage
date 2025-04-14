856a16219e6b6fbd8f97cfaa3b564db9dbd6f786
Abdellah <abdel.elmalky@gmail.com>
2025-04-14 16:33:57 +0100

    ajout de la table qui établit la relation many-to-many entre fichiers et tags

--- database/migrations/2025_04_14_151845_create_file_tag_table.php
+++ database/migrations/2025_04_14_151845_create_file_tag_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('file_tag', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('file_id')->constrained()->onDelete('cascade');
+            $table->foreignId('tag_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
