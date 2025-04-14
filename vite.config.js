d95844ac8235446520141281ccb0562983b732e8
Abdellah <abdel.elmalky@gmail.com>
2025-04-14 16:10:44 +0100

    modif de la config de vite pour activer la compilation des templates au moment de l'execution

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -13,8 +14,13 @@ export default defineConfig({
                 transformAssetUrls: {
                     base: null,
                     includeAbsolute: false,
-                }
-            }
+                },
+            },
         }),
     ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
 });
