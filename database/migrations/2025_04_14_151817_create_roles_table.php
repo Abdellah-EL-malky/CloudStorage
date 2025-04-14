6181b1c7597eea6b80cb357b1fab93e22a213ccf
Abdellah <abdel.elmalky@gmail.com>
2025-04-14 16:24:37 +0100

    ajout de la table role

--- database/migrations/2025_04_14_151817_create_roles_table.php
+++ database/migrations/2025_04_14_151817_create_roles_table.php
@@ -13,13 +13,8 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('storage_used')->default(0);
-            $table->integer('storage_limit')->default(1073741824); // 1GB
-            $table->rememberToken();
+            $table->string('name'); // admin, manager, collaborator, reader
+            $table->string('description');
             $table->timestamps();
         });
     }
