0e0538da41a41fc2fa81940711d71a215e445b2a
Abdellah <abdel.elmalky@gmail.com>
2025-04-28 17:05:23 +0100

    ajout de adminUserSeeder

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -4,14 +4,24 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Support\Facades\Hash;
 
 class AdminUserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        // Création de l'admin
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@cloudstorage.com',
+            'password' => Hash::make('password'),
+            'storage_limit' => 10737418240, // 10GB
+        ]);
+
+        // Assignation de l'admin
+        $adminRole = Role::where('name', 'admin')->first();
+        $user->roles()->attach($adminRole);
     }
 }
