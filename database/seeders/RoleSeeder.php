8621aa42dffc10d9c9411df949504b6006eb983b
Abdellah <abdel.elmalky@gmail.com>
2025-04-28 17:03:42 +0100

    ajout de roleseeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,14 +4,34 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Role;
 
 class RoleSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        // Création de rôles
+        $roles = [
+            [
+                'name' => 'admin',
+                'description' => 'Administrateur avec contrôle total sur le système'
+            ],
+            [
+                'name' => 'manager',
+                'description' => 'Gestionnaire pouvant créer des espaces et inviter des utilisateurs'
+            ],
+            [
+                'name' => 'collaborator',
+                'description' => 'Collaborateur pouvant modifier les fichiers partagés'
+            ],
+            [
+                'name' => 'reader',
+                'description' => 'Lecteur pouvant uniquement consulter les fichiers partagés'
+            ],
+        ];
+
+        foreach ($roles as $role) {
+            Role::create($role);
+        }
     }
 }
