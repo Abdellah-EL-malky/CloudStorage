33ca9af14fc345d23d04faa23eda17ec4e15381e
Abdellah <abdel.elmalky@gmail.com>
2025-04-28 15:55:46 +0100

    modification du model file

--- app/Models/File.php
+++ app/Models/File.php
@@ -8,4 +8,74 @@ use Illuminate\Database\Eloquent\Model;
 class File extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'path',
+        'mime_type',
+        'size',
+        'user_id',
+        'folder_id',
+        'is_favorite',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function folder()
+    {
+        return $this->belongsTo(Folder::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function shares()
+    {
+        return $this->morphMany(Share::class, 'shareable');
+    }
+
+    // Méthodes
+    public function getFormattedSizeAttribute()
+    {
+        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+        $size = $this->size;
+        $i = 0;
+
+        while ($size >= 1024 && $i < count($units) - 1) {
+            $size /= 1024;
+            $i++;
+        }
+
+        return round($size, 2) . ' ' . $units[$i];
+    }
+
+    public function isImage()
+    {
+        return strpos($this->mime_type, 'image/') === 0;
+    }
+
+    public function isPdf()
+    {
+        return $this->mime_type === 'application/pdf';
+    }
+
+    public function isVideo()
+    {
+        return strpos($this->mime_type, 'video/') === 0;
+    }
+
+    public function isAudio()
+    {
+        return strpos($this->mime_type, 'audio/') === 0;
+    }
 }
