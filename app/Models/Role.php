76d6a84c055be6f5646cd818b89f7dca3826b299
Abdellah <abdel.elmalky@gmail.com>
2025-04-28 14:22:54 +0100

    modification du model role

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    // Relation
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
