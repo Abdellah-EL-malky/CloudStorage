5f67eeb6415105b11c7ab1e8a8026915277a97cb
Abdellah <abdel.elmalky@gmail.com>
2025-05-01 14:49:04 +0100

    modification de la table user

---
+++ database/migrations/2025_05_01_134803_add_avatar_and_account_type_to_users.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('avatar_path')->nullable()->after('email');
+            $table->enum('account_type', ['free', 'premium', 'business'])->default('free')->after('avatar_path');
+            $table->dateTime('last_login')->nullable()->after('account_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
