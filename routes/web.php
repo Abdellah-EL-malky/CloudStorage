06596895af2ac60b16288babe2301b3eeaffcedd
Abdellah <abdel.elmalky@gmail.com>
2025-05-02 20:39:56 +0100

    ajout des routes d'authentification

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,42 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
+// Routes publiques
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+// Routes d'authentification
+Route::middleware('guest')->group(function () {
+    // Login
+    Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('login', [LoginController::class, 'login']);
+
+    // Register
+    Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('register', [RegisterController::class, 'register']);
+
+    // Forgot Password
+    Route::get('forgot-password', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+    Route::post('forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+
+    // Reset Password
+    Route::get('reset-password/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+    Route::post('reset-password', [ResetPasswordController::class, 'reset'])->name('password.update');
+});
+
+Route::middleware('auth')->group(function () {
+    // Logout
+    Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
+    // Plan selection
+    Route::get('plans', [RegisterController::class, 'showPlanSelectionForm'])->name('plans');
+    Route::post('plans', [RegisterController::class, 'selectPlan'])->name('plans.select');
+
 });
