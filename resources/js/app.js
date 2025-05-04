40df0adc175b747f9668fd9a6d949b7e34d2754f
Abdellah <abdel.elmalky@gmail.com>
2025-05-04 18:40:08 +0100

    installation des deépendances pour fontawesome et ajout de la librairie dans app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,14 @@ import { createApp } from 'vue';
 
 const app = createApp({});
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faCloudArrowDown } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCloudArrowDown);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
+
 import ExampleComponent from './components/ExampleComponent.vue';
 app.component('example-component', ExampleComponent);
 
