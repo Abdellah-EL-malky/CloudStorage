884c26202605af8eea711333626fd5299e55bed2
Abdellah <abdel.elmalky@gmail.com>
2025-05-05 09:55:51 +0100

    ajout des routes pour le paiement

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\PaymentController;
 
 
 // Routes publiques
@@ -39,4 +40,18 @@ Route::middleware('auth')->group(function () {
     Route::get('plans', [RegisterController::class, 'showPlanSelectionForm'])->name('plans');
     Route::post('plans', [RegisterController::class, 'selectPlan'])->name('plans.select');
 
+});
+
+// Routes de paiement
+Route::middleware('auth')->group(function () {
+    Route::get('/payment/{plan}', [PaymentController::class, 'show'])->name('payment.show');
+    Route::post('/payment/process', [PaymentController::class, 'process'])->name('payment.process');
+    Route::get('/payment/cancel', [PaymentController::class, 'cancel'])->name('payment.cancel');
+
+    // Simulation PayPal
+    Route::get('/payment/paypal/redirect', [PaymentController::class, 'redirectToPaypal'])->name('payment.paypal.redirect');
+    Route::get('/payment/paypal/success', [PaymentController::class, 'handlePaypalSuccess'])->name('payment.paypal.success');
+    Route::get('/payment/paypal/cancel', [PaymentController::class, 'handlePaypalCancel'])->name('payment.paypal.cancel');
+
+    Route::get('/payment/failed', [PaymentController::class, 'failed'])->name('payment.failed');
 });
