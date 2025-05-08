83e6ac0120ed0cf82100a827a82e5a8d2861e812
Abdellah <abdel.elmalky@gmail.com>
2025-05-08 14:15:53 +0100

    ajout du dashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,8 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\File;
+use App\Models\Folder;
+use App\Models\Share;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        $user = Auth::user();
+
+        $storageUsed = $user->storage_used;
+        $storageLimit = $user->storage_limit;
+        $storagePercentage = $user->getStoragePercentage();
+
+        $recentFiles = $user->files()->latest()->take(5)->get();
+
+        $recentShares = $user->shares()->latest()->take(5)->get();
+
+        $totalFiles = $user->files()->count();
+        $totalSharedFiles = $user->shares()->count();
+
+        return view('dashboard', compact(
+            'user',
+            'storageUsed',
+            'storageLimit',
+            'storagePercentage',
+            'recentFiles',
+            'recentShares',
+            'totalFiles',
+            'totalSharedFiles'
+        ));
+    }
 }
